eb7fdc7273f30faf78b6e3c1d32e25462318b68a
MagaSegu <136750615+MagaSegu@users.noreply.github.com>
2023-08-31 14:52:04 -0300

    agrego funcionalidad del banner

--- script.js
+++ script.js
@@ -6,24 +6,31 @@ console.log(key)
     switch (key) {
     case 49: // Tecla uno
         console.log('Se presionó el numero uno');
+        document.querySelector(".banner").classList.toggle("verBanner")
         break;
     case 50: // Tecla dos
         console.log('Se presionó el numero dos');
+        document.querySelector(".banner").classList.toggle("verBanner")
         break;
     case 51: // Tecla tres
         console.log('Se presionó el numero tres');
+        document.querySelector(".banner").classList.toggle("verBanner")
         break;
     case 52: // Tecla cuatro
         console.log('Se presionó el numero cuatro');
+        document.querySelector(".banner").classList.toggle("verBanner")
         break;
         case 53: // Tecla cinco
         console.log('Se presionó el numero cinco');
+        document.querySelector(".banner").classList.toggle("verBanner")
         break;
     case 54: // Tecla seis
         console.log('Se presionó el numero seis');
+        document.querySelector(".banner").classList.toggle("verBanner")
         break;
     case 55: // Tecla siete
         console.log('Se presionó el numero siete');
+        document.querySelector(".banner").classList.toggle("verBanner")
         break;
     }
 });
